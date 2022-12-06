5daa00614847c6d0d68bc3721a4f059d857aa618
AOYastrebov <AOYastrebov@gmail.com>
2022-12-06 22:05:46 +0200

    added phone_number, first and last name

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from django.forms import ModelForm
+from django.forms import ModelForm, EmailField, CharField
 from main.models import *
 from betterforms.multiform import MultiModelForm
 
@@ -24,7 +24,21 @@ class AddAnimalForm(MultiModelForm):
     }
 User = get_user_model()
 class UserCreationForm(UserCreationForm):
+    email = EmailField(label="Email")
+    phone_number = CharField(label="Phone number")
+
     
 
     class Meta(UserCreationForm.Meta):
         model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'phone_number')
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.phone_number = self.cleaned_data["phone_number"]
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+
