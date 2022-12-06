74077ec38a4b6a5108133057ad25e4dc994fdb6c
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-12-06 18:49:09 +0200

    Create add_animals.html templates with AddAnimal form Add upload view and upload.html to save Animal and Advert row in db

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path("", views.start, name="start"),
     path("add_animals/", views.add_animals_page, name="add_animals"),
--- main/views.py
+++ main/views.py
@@ -15,7 +15,11 @@ def start(response):
 def show_animals_page(response):
     animals = Animal.objects.all()
     adverts = Advert.objects.all()
-    return render(response, "homeless_animals/show_animals.html", {"animals": animals, "adverts": adverts})
+    return render(
+        response,
+        "homeless_animals/show_animals.html",
+        {"animals": animals, "adverts": adverts},
+    )
 
 
 def about_us_page(response):
@@ -24,14 +28,21 @@ def about_us_page(response):
 
 def add_animals_page(request):
     form = AddAnimalForm()
-    return render(request, 'homeless_animals/add_animals.html', {'form': form})
+    return render(request, "homeless_animals/add_animals.html", {"form": form})
 
 
 def add_animals_upload(request):
     body = request.body.decode().split("&")[1:]
     values = [i.split("=")[1] for i in body]
     admin = CustomUser.objects.filter(id=1).first()
-    animal = Animal.objects.create(species=values[0], size=values[1], sex=values[2], age=values[3], lost=values[4])
-    advert = Advert.objects.create(animal=animal, city=values[5], coordinates=values[6], description=values[7], inspector=admin)
-
+    animal = Animal.objects.create(
+        species=values[0], size=values[1], sex=values[2], age=values[3], lost=values[4]
+    )
+    advert = Advert.objects.create(
+        animal=animal,
+        city=values[5],
+        coordinates=values[6],
+        description=values[7],
+        inspector=admin,
+    )
     return render(request, "homeless_animals/upload.html")
