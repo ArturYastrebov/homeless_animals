ef8ec316b68aa0622b93890fb92bf1e12ea1df11
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-12-12 12:24:37 +0200

    Add url for show_animals/upload path

--- main/urls.py
+++ main/urls.py
@@ -5,8 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.start, name="start"),
     path("add_animals/", views.add_animals_page, name="add_animals"),
-    path("show_animals/", views.open_animals_page, name="show_animals"),
+    path("show_animals/", views.show_animals_page, name="show_animals"),
     path("about_us/", views.about_us_page, name="about_us"),
-    path("add_animals/upload/", views.add_animals_upload, name="upload"),
-    # path("show_animals/<id:int>", views.show_proper_animals, name="proper_animals")
-]
+    path("add_animals/upload/", views.add_animals_upload, name="add_animals_upload"),
+    path("show_animals/upload/", views.show_animals_upload, name="show_animals_upload"),
