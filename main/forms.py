5b15510cefac0a35cd4d70f56062c2e0e1e46e9c
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-12-12 12:30:09 +0200

    Delete AdvertForm and AnimalForm Create AddAnimalAdvertForm for add_animals.html page and FindAnimalAdvertForm for show_animals.html page

--- main/forms.py
+++ main/forms.py
@@ -1,22 +1,69 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Form
+from django import forms
 from main.models import *
 from betterforms.multiform import MultiModelForm
 
 
-class AnimalForm(ModelForm):
-    class Meta:
-        model = Animal
-        fields = ["species", "size", "sex", "age", "lost"]
+class FindAnimalAdvertForm(forms.Form):
+    SPECIES_CHOICES = [
+        ("DOG", "Dog"),
+        ("CAT", "Cat"),
+    ]
+    SIZE_CHOICES = [
+        ("L", "Big"),
+        ("M", "Middle"),
+        ("S", "Small"),
+        ("---", "---"),
+    ]
+    SEX_CHOICES = [("Male", "Male"), ("Female", "Female"), ("---", "---")]
+    AGE_CHOICES = [
+        ("JN", "Junior"),
+        ("MD", "Middle"),
+        ("SN", "Senior"),
+        ("---", "---"),
+    ]
+    LOST_CHOICES = [("YES", "Yes"), ("No", "No"), ("---", "---")]
+    CITY_CHOICES = [
+        ("KV", "Kyiv"),
+    ]
+
+    species = forms.ChoiceField(choices=SPECIES_CHOICES)
+    size = forms.ChoiceField(choices=SIZE_CHOICES)
+    sex = forms.ChoiceField(choices=SEX_CHOICES)
+    age = forms.ChoiceField(choices=AGE_CHOICES)
+    lost = forms.ChoiceField(choices=LOST_CHOICES)
+    city = forms.ChoiceField(choices=CITY_CHOICES)
 
 
-class AdvertForm(ModelForm):
-    class Meta:
-        model = Advert
-        fields = ["photo", "city", "coordinates", "description"]
+class AddAnimalAdvertForm(forms.Form):
+    STATUS_CHOICES = [("IN_PROCESS", "In process"), ("DONE", "Done")]
+    SPECIES_CHOICES = [
+        ("DOG", "Dog"),
+        ("CAT", "Cat"),
+    ]
+    SIZE_CHOICES = [
+        ("L", "Big"),
+        ("M", "Middle"),
+        ("S", "Small"),
+    ]
+    SEX_CHOICES = [("Male", "Male"), ("Female", "Female")]
+    AGE_CHOICES = [
+        ("JN", "Junior"),
+        ("MD", "Middle"),
+        ("SN", "Senior"),
+    ]
+    LOST_CHOICES = [("YES", "Yes"), ("No", "No")]
+    CITY_CHOICES = [
+        ("KV", "Kyiv"),
+    ]
 
-
-class AddAnimalForm(MultiModelForm):
-    form_classes = {
-        "animal": AnimalForm,
-        "advert": AdvertForm
-    }
+    # photo = models.FileField(null=True)
+    species = forms.ChoiceField(choices=SPECIES_CHOICES)
+    size = forms.ChoiceField(choices=SIZE_CHOICES)
+    sex = forms.ChoiceField(choices=SEX_CHOICES)
+    age = forms.ChoiceField(choices=AGE_CHOICES)
+    lost = forms.ChoiceField(choices=LOST_CHOICES)
+    city = forms.ChoiceField(choices=CITY_CHOICES)
+    coordinates = forms.CharField(max_length=50)
+    description = forms.CharField(widget=forms.Textarea)
+    photo = forms.FileField()
