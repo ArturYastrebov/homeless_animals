637ca7b1f56e92c566ea422702bcd71974a62adf
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-12-14 19:09:27 +0200

    Recreate AddAnimalForm depends on AnimalAdvert model

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,6 @@
-from django.forms import ModelForm, Form
+from django import forms
 from django import forms
 from main.models import *
-from betterforms.multiform import MultiModelForm
 
 
 class FindAnimalAdvertForm(forms.Form):
@@ -35,35 +34,17 @@ class FindAnimalAdvertForm(forms.Form):
     city = forms.ChoiceField(choices=CITY_CHOICES)
 
 
-class AddAnimalAdvertForm(forms.Form):
-    STATUS_CHOICES = [("IN_PROCESS", "In process"), ("DONE", "Done")]
-    SPECIES_CHOICES = [
-        ("DOG", "Dog"),
-        ("CAT", "Cat"),
-    ]
-    SIZE_CHOICES = [
-        ("L", "Big"),
-        ("M", "Middle"),
-        ("S", "Small"),
-    ]
-    SEX_CHOICES = [("Male", "Male"), ("Female", "Female")]
-    AGE_CHOICES = [
-        ("JN", "Junior"),
-        ("MD", "Middle"),
-        ("SN", "Senior"),
-    ]
-    LOST_CHOICES = [("YES", "Yes"), ("No", "No")]
-    CITY_CHOICES = [
-        ("KV", "Kyiv"),
-    ]
-
-    # photo = models.FileField(null=True)
-    species = forms.ChoiceField(choices=SPECIES_CHOICES)
-    size = forms.ChoiceField(choices=SIZE_CHOICES)
-    sex = forms.ChoiceField(choices=SEX_CHOICES)
-    age = forms.ChoiceField(choices=AGE_CHOICES)
-    lost = forms.ChoiceField(choices=LOST_CHOICES)
-    city = forms.ChoiceField(choices=CITY_CHOICES)
-    coordinates = forms.CharField(max_length=50)
-    description = forms.CharField(widget=forms.Textarea)
-    photo = forms.FileField()
+class AddAnimalAdvertForm(forms.ModelForm):
+    class Meta:
+        model = AnimalAdvert
+        fields = [
+            "city",
+            "coordinates",
+            "description",
+            "photo",
+            "species",
+            "size",
+            "sex",
+            "age",
+            "lost",
+        ]
