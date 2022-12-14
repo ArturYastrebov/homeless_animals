d2b6f06e9c20f5c5217afc505d8a357a16f5c069
KateSamus <109095866+KateSamus@users.noreply.github.com>
2022-12-14 19:09:02 +0200

    Delete Animal and Advert models Add AnimalAdvert model

--- main/models.py
+++ main/models.py
@@ -4,7 +4,14 @@ from django.db import models
 from datetime import datetime
 
 
-class Animal(models.Model):
+class CustomUser(AbstractUser):
+
+    phone_number = models.IntegerField(null=True)
+
+
+class AnimalAdvert(models.Model):
+    STATUS_CHOICES = [("IN_PROCESS", "In process"), ("DONE", "Done")]
+    CITY_CHOICES = [("KV", "Kyiv"), ]
     SPECIES_CHOICES = [
         ("DOG", "Dog"),
         ("CAT", "Cat"),
@@ -22,36 +29,19 @@ class Animal(models.Model):
     ]
 
     LOST_CHOICES = [("YES", "Yes"), ("No", "No")]
-    species = models.CharField(max_length=3, choices=SPECIES_CHOICES)
-    size = models.CharField(max_length=1, choices=SIZE_CHOICES)
-    sex = models.CharField(max_length=6, choices=SEX_CHOICES)
-    age = models.CharField(max_length=2, choices=AGE_CHOICES)
-    lost = models.CharField(max_length=3, choices=LOST_CHOICES)
-
-    def __str__(self):
-        return f"species: {self.species}\nsize: {self.size}\nsex: {self.sex}\nage: {self.age}\nlost: {self.lost}"
-
-
-class CustomUser(AbstractUser):
-
-    phone_number = models.IntegerField(null=True)
-
-
-class Advert(models.Model):
-    STATUS_CHOICES = [("IN_PROCESS", "In process"), ("DONE", "Done")]
-    CITY_CHOICES = [("KV", "Kyiv"), ]
 
     created = models.DateField(auto_now_add=True)
     last_updated = models.DateField(blank=True, default=timezone.now)
     status = models.CharField(
         max_length=10, choices=STATUS_CHOICES, default="IN_PROCESS"
     )
-    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
     city = models.CharField(max_length=50, choices=CITY_CHOICES)
     coordinates = models.CharField(max_length=50)
     description = models.TextField(null=True)
     inspector = models.ForeignKey(CustomUser, on_delete=models.RESTRICT, null=True)
-    photo = models.FileField(null=True)
-
-    def __str__(self):
-        return f" last_update: {self.last_updated}\ncity: {self.city}\ncoordinates\n{self.coordinates}\ndescription: {self.description}"
+    photo = models.ImageField(upload_to="images/", null=True)
+    species = models.CharField(max_length=3, choices=SPECIES_CHOICES)
+    size = models.CharField(max_length=1, choices=SIZE_CHOICES)
+    sex = models.CharField(max_length=6, choices=SEX_CHOICES)
+    age = models.CharField(max_length=2, choices=AGE_CHOICES)
+    lost = models.CharField(max_length=3, choices=LOST_CHOICES)
